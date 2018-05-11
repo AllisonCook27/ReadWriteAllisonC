98964662d338c61416a0d7938fa9c0e0a6fad795
Allison <allison.cook@ocsbstudent.ca>
2018-05-11 12:13:51 -0400

    Initial commit

---
+++ ReadWriteAllisonC/ReadWriteForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ReadWriteAllisonC
+{
+    partial class frmReadWrite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFile
+            // 
+            this.btnFile.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFile.Location = new System.Drawing.Point(105, 139);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(77, 30);
+            this.btnFile.TabIndex = 0;
+            this.btnFile.Text = "Check";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // frmReadWrite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(311, 261);
+            this.Controls.Add(this.btnFile);
+            this.Name = "frmReadWrite";
+            this.Text = "File read and write by Allison C";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFile;
+    }
+}
+
---
+++ ReadWriteAllisonC/ReadWriteForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 10 May, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #37 - String Equality with File read/write
+ * This program parses a file and reads it runs through the string equality code 
+ * Then out puts if the string is equal or not to a text file
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadWriteAllisonC
+{
+    public partial class frmReadWrite : Form
+    {
+        public frmReadWrite()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
