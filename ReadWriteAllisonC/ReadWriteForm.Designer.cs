b0a0560a26c50a4a364c38de23417d1de9d19c82
Allison <allison.cook@ocsbstudent.ca>
2018-05-11 12:53:56 -0400

    done

--- ReadWriteAllisonC/ReadWriteForm.Designer.cs
+++ ReadWriteAllisonC/ReadWriteForm.Designer.cs
@@ -29,12 +29,15 @@
         private void InitializeComponent()
         {
             this.btnFile = new System.Windows.Forms.Button();
+            this.lblDone = new System.Windows.Forms.Label();
+            this.lblCompare = new System.Windows.Forms.Label();
+            this.lblCase = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnFile
             // 
             this.btnFile.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFile.Location = new System.Drawing.Point(105, 139);
+            this.btnFile.Location = new System.Drawing.Point(109, 82);
             this.btnFile.Name = "btnFile";
             this.btnFile.Size = new System.Drawing.Size(77, 30);
             this.btnFile.TabIndex = 0;
@@ -42,22 +45,59 @@
             this.btnFile.UseVisualStyleBackColor = true;
             this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
             // 
+            // lblDone
+            // 
+            this.lblDone.AutoSize = true;
+            this.lblDone.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDone.Location = new System.Drawing.Point(23, 159);
+            this.lblDone.Name = "lblDone";
+            this.lblDone.Size = new System.Drawing.Size(276, 18);
+            this.lblDone.TabIndex = 1;
+            this.lblDone.Text = "Output has been printed on a text file.";
+            // 
+            // lblCompare
+            // 
+            this.lblCompare.AutoSize = true;
+            this.lblCompare.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCompare.Location = new System.Drawing.Point(12, 27);
+            this.lblCompare.Name = "lblCompare";
+            this.lblCompare.Size = new System.Drawing.Size(297, 18);
+            this.lblCompare.TabIndex = 2;
+            this.lblCompare.Text = "Compare strings on each line of a text file";
+            // 
+            // lblCase
+            // 
+            this.lblCase.AutoSize = true;
+            this.lblCase.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCase.Location = new System.Drawing.Point(38, 45);
+            this.lblCase.Name = "lblCase";
+            this.lblCase.Size = new System.Drawing.Size(148, 18);
+            this.lblCase.TabIndex = 3;
+            this.lblCase.Text = "Case doesn\'t matter";
+            // 
             // frmReadWrite
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(311, 261);
+            this.ClientSize = new System.Drawing.Size(316, 261);
+            this.Controls.Add(this.lblCase);
+            this.Controls.Add(this.lblCompare);
+            this.Controls.Add(this.lblDone);
             this.Controls.Add(this.btnFile);
             this.Name = "frmReadWrite";
             this.Text = "File read and write by Allison C";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Label lblDone;
+        private System.Windows.Forms.Label lblCompare;
+        private System.Windows.Forms.Label lblCase;
     }
 }
 
--- ReadWriteAllisonC/ReadWriteForm.cs
+++ ReadWriteAllisonC/ReadWriteForm.cs
@@ -23,11 +23,80 @@ namespace ReadWriteAllisonC
         public frmReadWrite()
         {
             InitializeComponent();
+
+            //hide done label
+            lblDone.Hide();
+        }
+
+        private bool StringsAreEqual(string string1, string string2)
+        {
+            //declare variable
+            bool isEqual;
+
+            //set the strings to upper case
+            string1 = string1.ToUpper();
+            string2 = string2.ToUpper();
+
+            //check if the strings are equal
+            if (string1 == string2)
+            {
+                //set the bool to true
+                isEqual = true;
+            }
+            else
+            {
+                //set the bool to false
+                isEqual = false;
+            }
+
+            //retrun the bool
+            return isEqual;
+
         }
 
         private void btnFile_Click(object sender, EventArgs e)
         {
 
+            //hide done label
+            lblDone.Hide();
+
+            //read each line into an array
+            //each element of the array is one line of the file
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+
+            //array of characters for when i split each line by spaces/ tabs
+            char[] charSaparators = new char[] { ' ', '\t' };
+
+            //the output where the results will go
+            string output = "";
+
+            //go through each line in the lines array
+            foreach (string line in lines)
+            {
+                //split each line by character and put them into an array
+                //for now it's only two words per line
+                string[] words = line.Split(charSaparators, StringSplitOptions.RemoveEmptyEntries);
+
+
+                //checking if the strings are equal
+                if (StringsAreEqual(words[0], words[1]) == true)
+                {
+                    //set the output to true and print it to a text file
+                    output = output + "True\r\n";
+                }
+                else
+                {
+                    //set the output to false and print it to a text file
+                    output = output + "False\r\n";
+                }
+
+            }
+
+            //creating the output file and printing the output
+            System.IO.File.WriteAllText(@"output.txt", output);
+
+            //showing the label saying the checking is done
+            lblDone.Show();
         }
     }
 }
